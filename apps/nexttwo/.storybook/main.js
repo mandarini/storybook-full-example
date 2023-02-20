39449770ff4045d980fda4da49d58539acad2a61
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-02-20 17:46:52 +0200

    remove uiFramework

---
+++ apps/nexttwo/.storybook/main.js
@@ -0,0 +1,14 @@
+const config = {
+  stories: ['../components/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+};
+
+export default config;
+
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
+// and https://nx.dev/packages/storybook/documents/custom-builder-configs
