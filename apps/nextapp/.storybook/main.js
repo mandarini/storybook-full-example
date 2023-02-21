30e7737888dd463704a60357f786eae61e678857
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-02-21 11:53:05 +0200

    udpated nextapp

--- apps/nextapp/.storybook/main.js
+++ apps/nextapp/.storybook/main.js
@@ -1,15 +1,19 @@
 const path = require('path');
-module.exports = {
-  stories: ['../components/**/*.stories.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@nrwl/react/plugins/storybook', 'storybook-addon-swc'],
+const config = {
+  stories: [
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/nextjs',
     options: {
-      nextConfigPath: '../next.config.js'
-    }
-  }
+      nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+    },
+  },
 };
 
+export default config;
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
 // and https://nx.dev/packages/storybook/documents/custom-builder-configs
